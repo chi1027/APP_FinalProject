114f78c3ab66475e568d3cca2d8ddf5e40048482
gaby20000413 <96484239+gaby20000413@users.noreply.github.com>
2023-06-07 21:23:46 +0800

    debug圖標消除

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -60,6 +60,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorSchemeSeed: const Color(0xff6750a4),
         useMaterial3: true,
